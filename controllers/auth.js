18fbe168b3b5f78f1e8344b0927db617183e1362
murat <muratorhun@yahoo.com>
2021-06-27 15:54:40 +0300

    Add validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 const { SENDGRID_API } = require('../util/personalData');
@@ -80,6 +81,14 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array(),
+    });
+  }
   User.findOne({ email: email })
     .then(userDoc => {
       //  Email already taken by a user
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -10,7 +11,33 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post(
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        if (value === 'fvck@test.com') {
+          throw new Error('This email adress is forbidden');
+        }
+        return true;
+      }),
+    body(
+      'password',
+      'Please enter a password with only numbers and text and between 5-16 characters .'
+    )
+      .isLength({ min: 5, max: 16 })
+      .isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+      if (!value === req.body.password) {
+        throw new Error('Passwords have to match!');
+      }
+      return true;
+    }),
+  ],
+  authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
